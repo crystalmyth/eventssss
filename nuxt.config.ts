896519e2cace9c5554e96c2c39a3b970510dee6a
crystalmyth <lakshayverma.lv@gmail.com>
2022-09-06 18:11:15 +0530

    sync

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: ['@pinia/nuxt'],
     css: ['~/assets/scss/main.scss'],
-    // buildModules: ['@pinia/nuxt'],
     vite: {
         css: {
             preprocessorOptions: {
